4d45f49cfc5275f0028d9806bc5fec0c6e484714
Jake Jasmin <roastbeefer000@gmail.com>
2023-05-09 17:49:02 -0600

    add stuff

---
+++ after/plugin/indent.lua
@@ -0,0 +1,9 @@
+--vim.opt.list = true
+
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    --char = "",
+    --show_current_context = true,
+    --show_current_context_start = true,
+    use_treesitter = true,
+}
