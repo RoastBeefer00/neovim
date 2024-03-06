46ed3e696b96e001022d16a0724751ac1f60eeed
Jake Jasmin <roastbeefer000@gmail.com>
2024-03-06 13:10:16 -0700

    fix K map

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,6 +133,7 @@ return {
                         local opts = { buffer = bufnr, remap = false }
 
                         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                        vim.keymap.set("n", "K", "I", opts)
                         vim.keymap.set("n", "<leader>H", function() vim.lsp.buf.hover() end, opts)
                         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
