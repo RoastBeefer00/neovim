acc77683d06a4f50daee9d068437ea31cc119ddb
Jake Jasmin <roastbeefer000@gmail.com>
2024-07-03 18:42:21 -0600

    golines

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 -- Conform will run multiple formatters sequentially
-                go = { "goimports", "gofmt" },
+                go = { "goimports", "gofmt", "golines" },
                 -- Use a sub-list to run only the first available formatter
                 javascript = { { "prettierd", "prettier" } },
                 nix = { "nixfmt" },
